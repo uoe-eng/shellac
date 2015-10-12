1984ca9820be3facac2547764f03c00ae5cfbf81
Matthew Richardson <M.Richardson@ed.ac.uk>
2015-10-12 09:54:40 +0100

    Update setup.py for pypi release.

--- setup.py
+++ setup.py
@@ -12,9 +12,15 @@ pkg_url = 'http://www.github.com/mrichar1/shellac'
 pkg_license = 'AGPL 3'
 pkg_description = "+shellac+ is an extension of the standard python library 'cmd', which aims to offer an alternative approach to nesting commands."
 pkg_author = 'Matthew Richardson, Bruce Duncan'
+pkg_author_email = 'm.richardson@ed.ac.uk'
 
-# List of python module dependencies
-# pip format: 'foo', 'foo==1.2', 'foo>=1.2' etc
+pkg_classifiers = [
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 3',
+            ]
+
 install_requires = []
 
 # Neither OSX nor Windows ship with GNU readline
@@ -23,29 +29,45 @@ if sys.platform == 'darwin':
 elif sys.platform.startswith('win'):
     install_requires.append('pyreadline>=2.0')
 
-# Do not edit below this line! #
 
-build_dir = "build/"
-dist_dir = "dist/"
-cur_dir = os.getcwd()
-
-def get_git_version(abbrev=4):
+def call_git_describe():
     try:
         p = subprocess.Popen(['git', 'describe'],
                              stdout=subprocess.PIPE)
         return p.communicate()[0].split('\n')[0].strip()
-    except Exception as e:
-        print e
+    except Exception:
         return None
 
-def main():
-    curdir = os.getcwd()
+
+def read_release_version():
     try:
-        os.mkdir(dist_dir)
-        os.mkdir(build_dir)
-    except OSError as exc:
-        if exc.errno != errno.EEXIST:
-            raise
+        with open("RELEASE-VERSION") as f:
+            return f.readlines()[0].strip()
+    except Exception:
+        return None
+
+
+def write_release_version(version):
+    with open("RELEASE-VERSION", "w") as f:
+        f.write("%s\n" % version)
+
+
+def get_git_version():
+    version = call_git_describe()
+    release_version = read_release_version()
+    if version is None:
+        version = release_version
+
+    if version is None:
+        raise ValueError("Unable to determine the version number!")
+
+    if version != release_version:
+        write_release_version(version)
+
+    return version
+
+
+def main():
 
     setuptools.setup(
         name=pkg_name,
@@ -53,11 +75,13 @@ def main():
         url=pkg_url,
         license=pkg_license,
         description=pkg_description,
+	    classifiers=pkg_classifiers,
         author=pkg_author,
+        author_email=pkg_author_email,
         packages=setuptools.find_packages('src'),
         package_dir={'': 'src'},
         include_package_data=True,
-        package_data = {'': ['LICENSE']},
+        package_data = {'': ['LICENSE', 'RELEASE-VERSION']},
         install_requires=install_requires,
         )
 
