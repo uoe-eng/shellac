fc744e66d537ea02b14183a6d7f477e6ecfd8e9d
Matthew Richardson <M.Richardson@ed.ac.uk>
2018-09-03 14:36:34 +0100

    Modify setup to use different versioning method.

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import setuptools
-from setuptools.command import easy_install
-import os
-import errno
-import re
-import subprocess
 import sys
+sys.path.append("src/shellac")
+import version
 
 # Change the following to represent your package:
 pkg_name = 'shellac'
@@ -30,48 +27,11 @@ elif sys.platform.startswith('win'):
     install_requires.append('pyreadline>=2.0')
 
 
-def call_git_describe():
-    try:
-        p = subprocess.Popen(['git', 'describe'],
-                             stdout=subprocess.PIPE)
-        return p.communicate()[0].decode().strip()
-    except Exception:
-        return None
-
-
-def read_release_version():
-    try:
-        with open("RELEASE-VERSION") as f:
-            return f.readlines()[0].strip()
-    except Exception:
-        return None
-
-
-def write_release_version(version):
-    with open("RELEASE-VERSION", "w") as f:
-        f.write("%s\n" % version)
-
-
-def get_git_version():
-    version = call_git_describe()
-    release_version = read_release_version()
-    if version is None:
-        version = release_version
-
-    if version is None:
-        raise ValueError("Unable to determine the version number!")
-
-    if version != release_version:
-        write_release_version(version)
-
-    return version
-
-
 def main():
 
     setuptools.setup(
         name=pkg_name,
-        version=get_git_version(),
+        version=version.get_version(),
         url=pkg_url,
         license=pkg_license,
         description=pkg_description,
@@ -81,7 +41,7 @@ def main():
         packages=setuptools.find_packages('src'),
         package_dir={'': 'src'},
         include_package_data=True,
-        package_data = {'': ['LICENSE', 'RELEASE-VERSION']},
+        package_data = {'': ['LICENSE']},
         install_requires=install_requires,
         test_suite="{0}.{1}".format(pkg_name, "tests"),
         )
---
+++ src/shellac/version.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""Generate version information from git tags, archive info, or PKG-INFO.
+
+Based on: https://github.com/Changaco/version.py
+
+"""
+
+from os.path import dirname, isdir, isfile, join
+import re
+from subprocess import CalledProcessError, check_output
+import pkg_resources
+
+
+def get_version():
+    """Get version in any way possible."""
+
+    prefix = ''
+    tag_re = re.compile(r'\btag: %s([0-9][^,]*)\b' % prefix)
+    version_re = re.compile('^Version: (.+)$', re.M)
+
+    # Return the version if it has been injected into the file by git-archive
+    version = tag_re.search('$Format:%D$')
+    if version:
+        return version.group(1)
+
+    project_dir = dirname(__file__)
+    pkg_file = join(project_dir, '../PKG-INFO')
+
+    if isdir(join(project_dir, '../.git')):
+        # Get the version using "git describe".
+        cmd = 'git -C %s describe --tags --match %s[0-9]* --dirty' % (project_dir, prefix)
+        try:
+            version = check_output(cmd.split()).decode().strip()[len(prefix):]
+        except CalledProcessError:
+            raise RuntimeError('Unable to get version number from git tags')
+
+        # PEP 440 compatibility
+        if '-' in version:
+            version = '.dev'.join(version.split('-')[:2])
+        return version
+
+    if isfile(pkg_file):
+        # Extract the version from the PKG-INFO file.
+        with open(pkg_file) as info_f:
+            return version_re.search(info_f.read()).group(1)
+
+    # Package was installed (os pkg, pip etc) without PKG-INFO
+    return pkg_resources.get_distribution('shellac').version
+
+
+if __name__ == '__main__':
+    print(get_version())
